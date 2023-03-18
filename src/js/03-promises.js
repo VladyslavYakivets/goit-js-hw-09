ab62c78b9edb0d4302060733af8a5dfae15a943c
VladyslavYakivets <aquartis97@gmail.com>
2023-03-18 12:13:14 +0200

    завдання 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import Notiflix from "notiflix";
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise ((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay) 
+  });  
 }
+
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+  const time = Number(formEl.delay.value);
+  const step = Number(formEl.step.value);
+  const amount = Number(formEl.amount.value);
+  let position = 1;
+  let delay = time;
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(position, delay).then(x => Notiflix.Notify.success(x)).catch(y => Notiflix.Notify.failure(y));
+    position += 1;
+    delay += step;
+  }
+};
