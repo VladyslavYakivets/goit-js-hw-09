b0e1251300ddd933a223ad63f140e42f7be88638
VladyslavYakivets <114339272+VladyslavYakivets@users.noreply.github.com>
2023-03-18 07:57:58 +0000

    Deploying to gh-pages from @ VladyslavYakivets/goit-js-hw-09@a7f248ffff261397efbe20bf305ae06e7055abea 🚀

---
+++ 01-color-switcher.d00578c1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");document.querySelector("body");let r=null;e.addEventListener("click",(function(){r=setInterval(getBgColor,1e3),e=setAttribute("disabled")})),t.addEventListener("click",(function(){clearInterval(r),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.d00578c1.js.map
